bb4d4516b0015c53fbab09b946485b68b042f6ab
aswin <gokul.rox02@gmail.com>
2023-09-13 13:19:16 +0530

    commit all

---
+++ PatternMatching/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace PatternMatching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //reference variable
+            ParentClass pc;
+
+            //object of child
+            pc = new ChildClass() { x = 10, y = 20 };
+
+            //acess parent class
+
+          if(pc is ChildClass)
+            {
+                ChildClass cc = (ChildClass)pc;
+                Console.WriteLine(cc.x);
+                Console.WriteLine(cc.y);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
